904da600ced79c1f8da41a11fe25e9985fbdb06b
juyeon522 <50095417+juyeon522@users.noreply.github.com>
2021-02-22 15:27:32 +0900

    포인트 추가

--- HP/board/boardSubmit.php
+++ HP/board/boardSubmit.php
@@ -1,19 +1,20 @@
 <?php
-
-    error_reporting(E_ALL);
-
-    ini_set("display_errors", 1);
-
-
+    //error_reporting(E_ALL);
+	session_start();
+	$userid = $_SESSION["userid"];
+	
+    //ini_set("display_errors", 1);
 	$id = $_POST['userid'];
 	$title = $_POST['title'];
 	$content = $_POST['content'];
     $mode = $_POST['mode'];
     $boardID = $_POST['boardID'];
 	$datetime = date("Y-m-d");
+	
 
     
 	include "../lib/dbconn.php";
+	
 	
 	if($title != null) {
         if($content != null) {
@@ -21,22 +22,32 @@
  
                 case 0:
                     $sql = "INSERT INTO board(title, boardContent, boardHit, boardDatetime, userID) VALUES('$title','$content',0,'$datetime','$id');";
-                    mysqli_query($conn,$sql);
-               
+					mysqli_query($conn,$sql);
+					
+					$point_up = 10;
+					$sql = "SELECT point from user WHERE id = '$userid'";
+					$result = mysqli_query($conn,$sql);
+					$row = mysqli_fetch_array($result);
+					$new_point = $row["point"] + $point_up;
+					
+                    $sql = "UPDATE user SET point = $new_point WHERE id = '$userid'";
+					
+					mysqli_query($conn,$sql);
+					
                     mysqli_close($conn);
-                    echo ("
-                    <script>
-                        location.href = './boardList.php';
-                    </script>
-                    ");
-                    break;
 
-                  
+                    echo ("
+					<script>
+						location.href = './boardList.php';
+					</script>
+					");
+                    break;
+					
                     
                 case 1: 
                     $sql = "UPDATE board SET title = '$title', boardContent = '$content' WHERE boardID = $boardID;";
                     
-                     mysqli_query($conn,$sql);
+                    mysqli_query($conn,$sql);
                     mysqli_close($conn);
 
                     echo ("
@@ -72,5 +83,4 @@
         
     }
         
-    
 ?>
